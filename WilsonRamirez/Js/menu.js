6567441ec4aeaf484020a456391389f80586280e
WilsonRamirez-ESPE <wdramirez@espe.edu.ec>
2024-06-08 15:11:39 -0500

    Update Script

--- WilsonRamirez/Js/menu.js
+++ WilsonRamirez/Js/menu.js
@@ -53,7 +53,7 @@ class SuperNav extends HTMLElement {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" 
                 integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" 
                 crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <link rel="stylesheet" href="estilo.css">
+            <link rel="stylesheet" href="estilosP.css">
             <div class="contenedor-header">
                 <header>
                     <h1>${textThe} <span class="txtRojo">${textGym}</span></h1>
