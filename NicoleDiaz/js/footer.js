5a1f693c5ab3771edceeda35fc2f582591b47d6f
Nicole352 <75482188+Nicole352@users.noreply.github.com>
2024-06-08 15:28:35 -0500

    n

---
+++ NicoleDiaz/js/footer.js
@@ -0,0 +1,38 @@
+class SuperFooter extends HTMLElement {
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        const year = this.getAttribute('data-year') || '2023';
+        const company = this.getAttribute('data-company') || 'THE GYM';
+        const redes = this.getAttribute('data-redes') || '';
+
+        const redesItems = redes.split(',').map(item => {
+            const [name, url] = item.split('|');
+            return `<a href="${url.trim()}"><i class="fa-brands fa-${name.trim()}"></i></a>`;
+        }).join('');
+
+        this.shadowDOM.innerHTML = `
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" 
+                integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" 
+                crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <link rel="stylesheet" href="estilo.css">
+            <footer>
+                <div class="info">
+                    <p>${year} - <span class="txtRojo">${company}</span> Todos los derechos reservados</p>
+                    <div class="redes">
+                        ${redesItems}
+                    </div>
+                </div>
+            </footer>
+        `;
+    }
+}
+
+window.customElements.define('super-footer', SuperFooter);
