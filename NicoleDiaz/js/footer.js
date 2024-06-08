9a27a4e0acb364a1f50f990094ec85e97f3a4e82
WilsonRamirez-ESPE <wdramirez@espe.edu.ec>
2024-06-08 15:24:54 -0500

    Update Script

--- NicoleDiaz/js/footer.js
+++ NicoleDiaz/js/footer.js
@@ -22,7 +22,7 @@ class SuperFooter extends HTMLElement {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" 
                 integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" 
                 crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <link rel="stylesheet" href="estilosP.css">
+            <link rel="stylesheet" href="estilo.css">
             <footer>
                 <div class="info">
                     <p>${year} - <span class="txtRojo">${company}</span> Todos los derechos reservados</p>
